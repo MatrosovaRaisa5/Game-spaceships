b3ed7626dc7953c79701d31ccdcc0d5ea8a58944
Raisa Matrosova <matrosova-05@list.ru>
2024-10-21 22:24:32 +0630

    Commit message

--- OOAIP/SpaceBattle.Lib/Move.cs
+++ OOAIP/SpaceBattle.Lib/Move.cs
@@ -2,7 +2,28 @@
 
 public interface IMoving
 {
-    int[] Position { get; set; }
-    int[] Position { get; }
+    public int[] Position { get; set; }
+    public int[] Velocity { get; }
+}
 
+public interface ICommand
+{
+    void Execute();
+}
+
+public class MoveCommand : ICommand
+{
+    private IMoving obj;
+    public MoveCommand(IMoving obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.Position = new int[] {
+            obj.Position[0] + obj.Velocity[0],
+            obj.Position[1] + obj.Velocity[1],
+        };
+
+    }
 }
--- OOAIP/SpaceBattle.Tests/MoveCommandTest.cs
+++ OOAIP/SpaceBattle.Tests/MoveCommandTest.cs
@@ -1,10 +1,20 @@
-namespace SpaceBattle.Tests;
+﻿namespace SpaceBattle.Lib.Tests;
+using Moq;
 
 public class MoveCommandTest
 {
     [Fact]
-    public void Test1()
+    public void MoveCommandPositive()
     {
 
+        var moving = new Mock<IMoving>();
+        moving.SetupGet(m => m.Position).Returns(new int[] { 12, 5 });
+        moving.SetupGet(m => m.Velocity).Returns(new int[] { -3, 3 });
+        var mc = new MoveCommand(moving.Object);
+
+        mc.Execute();
+
+
+        moving.VerifySet(m => m.Position = new int[] { 9, 8 });
     }
 }
