a65b710451abd3a94af6cc4396e3018f486c0a55
Raisa Matrosova <matrosova-05@list.ru>
2024-10-21 20:09:13 +0630

    Commit Message

---
+++ OOAIP/SpaceBattle.Lib/Move.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMoving
+{
+    int[] Position { get; set; }
+    int[] Position { get; }
+
+}
--- OOAIP/SpaceBattle.Tests/MoveCommandTest.cs
+++ OOAIP/SpaceBattle.Tests/MoveCommandTest.cs
@@ -1,6 +1,6 @@
 namespace SpaceBattle.Tests;
 
-public class UnitTest1
+public class MoveCommandTest
 {
     [Fact]
     public void Test1()
